778dba89aa6a0fefae7ddeac3a2e55275a7afa93
anastasiia <anastasiia.peleshok@gmail.com>
2025-02-05 21:19:49 +0000

    first commit

---
+++ src/main/java/com/example/sharagasystem/SharagaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharagasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharagaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharagaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sharagasystem/SharagaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharagasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharagaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
